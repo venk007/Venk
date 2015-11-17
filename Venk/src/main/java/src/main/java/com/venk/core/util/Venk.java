de909a2f76d7e02969f8220050a775b0399c94e8
venk007 <liwenkeq@sina.com>
2015-11-17 20:30:30 +0800

    Add QuickSort
    2015-11-17
    
    Signed-off-by: venk007 <liwenkeq@sina.com>

--- Venk/src/main/java/src/main/java/com/venk/core/util/Venk.java
+++ Venk/src/main/java/src/main/java/com/venk/core/util/Venk.java
@@ -39,6 +39,39 @@ public class Venk {
 				index[x] = 0;
 		}
 	}
+	
+	public static void quickSort(int arr[],int low,int high){
+		
+		int l = low;
+		int h = high;
+		int povit = arr[low];
+		
+		while(l < h){
+			while( l < h && arr[h] >= povit)
+				h--;
+			if(l < h){
+				int temp = arr[h];
+				arr[h] = arr[l];
+				arr[l] = temp;
+				l++;
+			}
+			while(l < h && arr[l] <= povit)
+				l++;
+			if(l < h){
+				int temp = arr[h];
+				arr[h] = arr[l];
+				arr[l] = temp;
+				h--;
+			}
+		}
+		// print(arr);
+		System.out.println("l=" + (l+1) + "h=" + (h+1) + "povit=" + povit + "\n");
+		if(l>low)
+			quickSort(arr,low,l-1);
+		if(h<high)
+			quickSort(arr,l+1,high);
+		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
