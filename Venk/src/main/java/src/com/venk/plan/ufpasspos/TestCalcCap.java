e6ca45a2db4e4976a97dce4ba82cbeae8458c57e
venk007 <liwenkeq@sina.com>
2015-12-03 18:29:06 +0800

    Signed-off-by: venk007 <liwenkeq@sina.com>

--- Venk/src/main/java/src/com/venk/plan/ufpasspos/TestCalcCap.java
+++ Venk/src/main/java/src/com/venk/plan/ufpasspos/TestCalcCap.java
@@ -1,11 +1,19 @@
 package src.com.venk.plan.ufpasspos;
 
+/**
+ * 
+ * @Description: CalcCap测试类
+ * @author: Venk007
+ * @date: 2015年12月3日下午6:28:06 
+ *
+ */
 public class TestCalcCap {
 
 	public static void main(String[] args) {
 		
 		StringBuffer idNumBuf = new StringBuffer();
-		idNumBuf.append("34052419800101001");
+		//idNumBuf.append("34052419800101001");
+		idNumBuf.append("33032919850223958");
 		System.out.println(CalcCap.calcCap(idNumBuf));
 
 
