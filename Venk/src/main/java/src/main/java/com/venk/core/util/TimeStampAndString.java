b47b87085800b428c5b010936bd642707c619c70
venk007 <liwenkeq@sina.com>
2016-03-28 17:23:21 +0800

    添加 TimeStamp 与 String 格式互转工具类
    Signed-off-by: venk007 <liwenkeq@sina.com>

---
+++ Venk/src/main/java/src/main/java/com/venk/core/util/TimeStampAndString.java
@@ -0,0 +1,97 @@
+package src.main.java.com.venk.core.util;
+
+import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @Description: TimeStamp 与 String 类型的日期互相转换
+ * @author: Venk007
+ * @date: 2016年3月28日下午5:03:43 
+ *
+ */
+public class TimeStampAndString {
+
+	/**
+	 * TimeStamp转换成String格式(YYYY-MM-DD)
+	 * 
+	 * @param Timestamp
+	 * @return String
+	 * @throws Exception
+	 */
+	public static String timeToStringDate(Timestamp ts) throws Exception {
+
+		DateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		try {
+			if (ts != null) {
+				return sdf.format(ts);
+			}
+		} catch (Exception e) {
+			/*logger.warn("TimeStamp转换String格式出错", e);*/
+			System.out.println("TimeStamp转换String格式出错");
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	/**
+	 * TimeStamp转换成String格式(YYYY-MM-DD HH:MM:SS)
+	 * 
+	 * @param Timestamp
+	 * @return String
+	 * @throws Exception
+	 */
+	public static String timeToStringDateTime(Timestamp opTime) {
+		DateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		try {
+			if (opTime != null) {
+				return sdf.format(opTime);
+			}
+		} catch (Exception e) {
+			System.out.println("TimeStamp转换String格式出错");
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	/**
+	 * String格式(YYYY-MM-DD)转换成TimeStamp
+	 * 
+	 * @param Timestamp
+	 * @return String
+	 * @throws Exception
+	 */
+	public static Timestamp stringToTime(String beginDateIn) throws Exception {
+
+		String beginDateNew = "";
+		if ((beginDateIn != null) && (!"".equals(beginDateIn))) {
+			try {
+				beginDateNew = beginDateIn.concat(" 00:00:00"); // 默认添加时分秒为0时0分0秒
+				Timestamp beginDate = new Timestamp(System.currentTimeMillis());
+				beginDate = Timestamp.valueOf(beginDateNew);
+				return beginDate;
+			} catch (Exception e) {
+				System.out.println("String日期格式转换TimeStamp异常");
+				e.printStackTrace();
+			}
+		}
+		return null;
+	}
+	
+	
+	public static void main(String[] args) throws Exception {
+		
+		Date date = new Date();
+		Timestamp time = new Timestamp(date.getTime());
+		String dateStr = timeToStringDate(time);
+		String dateTimeStr = timeToStringDateTime(time);
+		Timestamp strToTime = stringToTime(dateStr);
+		
+		System.out.println("dateStr: \n" + dateStr);
+		System.out.println("\ndateTimeStr: \n" + dateTimeStr);
+		System.out.println("\nstrToTime: \n" + strToTime);
+		
+	}
+
+}
